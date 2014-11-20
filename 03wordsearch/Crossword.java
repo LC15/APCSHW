9cf1fc1bbdea0b6e1ce76937f8f288862de193b0
LC15 <schowdhury12@aol.com>
2014-11-20 12:22:52 -0500

    word puzzle

---
+++ 03wordsearch/Crossword.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Crossword{
+    public static void setSize(){
+	System.out.println("What dimensions would you like this puzzle to be? (Enter  the dimensions in the following format: length x width)");
+	Scanner size=new Scanner(System.in);
+	String dim=size.nextLine();
+    }}
+
